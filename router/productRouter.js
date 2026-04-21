da565b61f091ac216f84c45e3177b92af6030a96
SenalAbeysekara <senaldushyantha@gmail.com>
2026-04-21 15:56:35 +0530

    Last Day

--- router/productRouter.js
+++ router/productRouter.js
@@ -1,14 +1,13 @@
 import express from "express";
-import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from "../controllers/productController.js";
+import { createProduct, deleteProduct, getProductById, getProducts, searchProducts, updateProduct } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/", createProduct);
 productRouter.get("/", getProducts);
-productRouter.get("/trending", (req,res)=>{
-    res.status(200).json({message:"This is trending products endpoint"})
-})
+productRouter.get("/search/:query", searchProducts);
 productRouter.delete("/:productId", deleteProduct);
 productRouter.put("/:productId", updateProduct);
 productRouter.get("/:productId",getProductById)
+
 export default productRouter;
